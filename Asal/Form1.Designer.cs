e0c33569edde67e270acfad745663495b859bfec
ardaozkal <ardaozkal@live.com>
2015-05-09 01:09:18 +0300

    Main Code Release

---
+++ Asal/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Asal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(78, 13);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(215, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(78, 40);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(215, 20);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Başlangıç: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bitiş:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Üzerinde çalışılan: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Bulunan En Büyük: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(260, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Max teknik sınır: 79228162514264337593543950335";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(300, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(158, 121);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Bulunan Asal Sayı Miktarı:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 134);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Çalışma Süresi: ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 171);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Asal Sayı Hesaplayıcısı -Arda Özkal";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+    }
+}
+
---
+++ Asal/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asal
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bw = new BackgroundWorker();
+        List<decimal> asallar = new List<decimal>();
+        decimal bulunan = 0;
+        DateTime starttime;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bw.WorkerSupportsCancellation = true;
+            bw.WorkerReportsProgress = true;
+            bw.DoWork += bw_DoWork;
+            bw.ProgressChanged += bw_ProgressChanged;
+        }
+
+        void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.UserState.ToString().StartsWith("Üzerinde çalışılan: "))
+           {
+               label3.Text = e.UserState.ToString();
+           }
+           else if (e.UserState.ToString() == "Finish")
+           {
+               button1.Enabled = true;
+               timer1.Stop();
+               MessageBox.Show("Bitti ve " + (DateTime.Now - starttime).ToString("G") + " sürdü, ayrıca " + bulunan + " asal sayı bulundu.");
+           }
+           else
+           {
+               asallar.Add(Convert.ToDecimal(e.UserState));
+               //numericUpDown1.Value = Convert.ToDecimal(e.UserState);
+               listBox1.Items.Add(Convert.ToDecimal(e.UserState));
+               label4.Text = "Bulunan En Büyük: " + e.UserState.ToString();
+               bulunan++;
+               label6.Text = "Bulunan Asal Sayı Miktarı: " + bulunan;
+           }
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            decimal from = Convert.ToDecimal(e.Argument.ToString().Substring(0, e.Argument.ToString().IndexOf("-*-*-*-")));
+            decimal to = Convert.ToDecimal(e.Argument.ToString().Replace(from + "-*-*-*-", ""));
+
+            if (from < 2)
+            {
+                bw.ReportProgress(0, 2);
+            }
+            if (from < 3)
+            {
+                bw.ReportProgress(0, 3);
+            }
+            if (from < 5)
+            {
+                bw.ReportProgress(0, 5);
+            }
+            if (from < 7)
+            {
+                bw.ReportProgress(0, 7);
+            }
+            if (from < 11)
+            {
+                bw.ReportProgress(0, 11);
+            }
+
+            for (decimal i = from; i <= to; i++)
+            {
+                bw.ReportProgress(0, "Üzerinde çalışılan: " + i);
+                if ((i % 3 != 0) && (i % 2 != 0) && (i % 5 != 0) && (i % 7 != 0) && (i % 11 != 0))
+                {
+                    bool hicciktimi = false;
+                    for (decimal j = 2; j < i - 2; j++)
+                    {
+                        if (i % (j + 1) == 0)
+                        {
+                            hicciktimi = true;
+                            break;
+                        }
+                    }
+                    if (hicciktimi == false)
+                    {
+                        if (i != 0 && i != 1)
+                        {
+                            bw.ReportProgress(0, i);
+                        }
+                    }
+                }
+            }
+            bw.ReportProgress(0, "Finish");
+        }
+
+        //void bw_DoWork(object sender, DoWorkEventArgs e)
+        //{
+        //    decimal from = Convert.ToDecimal(e.Argument.ToString().Substring(0, e.Argument.ToString().IndexOf("-*-*-*-")));
+        //    decimal to = Convert.ToDecimal(e.Argument.ToString().Replace(from + "-*-*-*-", ""));
+
+        //    if (from < 2)
+        //    {
+        //        bw.ReportProgress(0, 2);
+        //    }
+        //    if (from < 3)
+        //    {
+        //        bw.ReportProgress(0, 3);
+        //    }
+        //    if (from < 5)
+        //    {
+        //        bw.ReportProgress(0, 5);
+        //    }
+        //    if (from < 7)
+        //    {
+        //        bw.ReportProgress(0, 7);
+        //    }
+        //    if (from < 11)
+        //    {
+        //        bw.ReportProgress(0, 11);
+        //    }
+
+        //    for (decimal i = from; i <= to; i++)
+        //    {
+        //        bw.ReportProgress(0, "Working on: " + i);
+        //        if ((i % 3 != 0) && (i % 2 != 0) && (i % 5 != 0) && (i % 7 != 0) && (i % 11 != 0))
+        //        {
+        //            bool hicciktimi = false;
+        //            for (decimal j = 2; j < i - 2; j++)
+        //            {
+        //                if (i % (j + 1) == 0)
+        //                {
+        //                    hicciktimi = true;
+        //                    break;
+        //                }
+        //            }
+        //            if (hicciktimi == false)
+        //            {
+        //                if (i != 0 && i != 1)
+        //                {
+        //                    bw.ReportProgress(0, i);
+        //                }
+        //            }
+        //        }
+        //    }
+        //    bw.ReportProgress(0, "Finish");
+        //}
+
+        //void bw_DoWork(object sender, DoWorkEventArgs e)
+        //{
+        //    decimal from = Convert.ToDecimal(e.Argument.ToString().Substring(0, e.Argument.ToString().IndexOf("-*-*-*-")));
+        //    decimal to = Convert.ToDecimal(e.Argument.ToString().Replace(from + "-*-*-*-", ""));
+
+        //    for (decimal i = from; i <= to; i++)
+        //    {
+        //        bw.ReportProgress(0, "Working on: " + i);
+        //        decimal sonuc = 0;
+        //        for (int z = 0; z <= i.ToString().Length - 1; z++)
+        //        {
+        //            sonuc += decimal.Parse(i.ToString().Substring(z, 1));
+        //        }
+        //        if ((sonuc % 3) != 0)
+        //        {
+        //            if (!(i.ToString().EndsWith("0") || i.ToString().EndsWith("2") || i.ToString().EndsWith("4") || i.ToString().EndsWith("6") || i.ToString().EndsWith("8")))
+        //            {
+        //                bool hicciktimi = false;
+        //                for (decimal j = 2; j < i - 2; j++)
+        //                {
+        //                    if (i % (j + 1) == 0)
+        //                    {
+        //                        hicciktimi = true;
+        //                        break;
+        //                    }
+        //                }
+        //                if (hicciktimi == false)
+        //                {
+        //                    if (i != 0 && i != 1)
+        //                    {
+        //                        bw.ReportProgress(0, i);
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    bw.ReportProgress(0, "Finish");
+        //}
+
+        //void bw_DoWork(object sender, DoWorkEventArgs e)
+        //{
+        //    decimal from = Convert.ToDecimal(e.Argument.ToString().Substring(0, e.Argument.ToString().IndexOf("-*-*-*-")));
+        //    decimal to = Convert.ToDecimal(e.Argument.ToString().Replace(from + "-*-*-*-", ""));
+
+        //    for (decimal i = from; i <= to; i++)
+        //    {
+        //        bw.ReportProgress(0, "Working on: " + i);
+        //        decimal sonuc = 0;
+        //        for (int z = 0; z <= i.ToString().Length - 1; z++)
+        //        {
+        //            sonuc += decimal.Parse(i.ToString().Substring(z, 1));
+        //        }
+        //        if ((sonuc % 3) != 0)
+        //        {
+        //            if (!(i.ToString().EndsWith("0") || i.ToString().EndsWith("2") || i.ToString().EndsWith("4") || i.ToString().EndsWith("6") || i.ToString().EndsWith("8")))
+        //            {
+        //                bool hicciktimi = false;
+        //                for (decimal j = 0; j < i - 2; j++)
+        //                {
+        //                    decimal x = i / (j + 1);
+        //                    if (!(x.ToString().Contains(".") || x.ToString().Contains(",") || x == i))
+        //                    {
+        //                        hicciktimi = true;
+        //                        break;
+        //                    }
+        //                }
+        //                if (hicciktimi == false)
+        //                {
+        //                    if (i != 0 && i != 1)
+        //                    {
+        //                        bw.ReportProgress(0, i);
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    bw.ReportProgress(0, "Finish");
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bw.IsBusy != true)
+            {
+                bulunan = 0;
+                starttime = DateTime.Now;
+                timer1.Start();
+                string str = (numericUpDown1.Value + "-*-*-*-" + numericUpDown2.Value);
+                bw.RunWorkerAsync(str);
+
+                button1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string texttogo = "Çalışma Süresi: " + (DateTime.Now - starttime).ToString("G");
+            label7.Text = texttogo;
+        }
+
+        string icerik = "";
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            icerik = "";
+            foreach (decimal str in listBox1.Items)
+            {
+                icerik += str.ToString() + Environment.NewLine;
+            }
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            System.IO.File.WriteAllText(saveFileDialog1.FileName, icerik);
+        }
+    }
+}
